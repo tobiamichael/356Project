6f344772e91a5f8c4e7e687994c2bc3410e581ed
tomlord69 <tlord_home@hotmail.com>
2019-10-18 12:53:58 +1100

    added respawn
    respawn

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+    public const int maxHealth = 1;
+    [SyncVar]public int currentHealth = maxHealth;
+    private NetworkStartPosition[] spawnPoints;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if(!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            currentHealth = maxHealth;
+            RpcRespawn();
+        }
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            Vector3 spawnPoint = Vector3.zero;
+
+            if (spawnPoints != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+
+            transform.position = spawnPoint;
+        }
+    }
+}
---
+++ Assets/Scripts/TestCannonBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCannonBall : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+    {
+        GameObject hit = collision.gameObject;
+        Health health = hit.GetComponent<Health>();
+
+        if(health != null)
+        {
+            health.TakeDamage(1);
+        }
+    }
+}
