6203616c87d97d556eb2d4e6f82f11236ed28612
Jordan <38971636+jordanABurns@users.noreply.github.com>
2019-10-19 15:38:14 +1100

    power ups for players

---
+++ Assets/PULog.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PULog : MonoBehaviour {
+
+    public GameObject pickupEffect;
+
+    public GameObject logObject;
+
+    Vector3 heightOffset = new Vector3(0, 10, 0);
+    Quaternion rotationOffset = Quaternion.Euler(0, 0, 90);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup(Collider player)
+    {
+        GameObject effect = Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        Destroy(effect, 1.0f);
+
+        GameObject log = Instantiate(logObject, player.transform.position + heightOffset, rotationOffset);
+
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/roll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roll : MonoBehaviour {
+
+    public float translateSpeed = 5f;
+    public float rotationSpeed = 360f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0,0,-translateSpeed * Time.deltaTime);
+
+        transform.GetChild(0).Rotate(Vector3.up * (rotationSpeed * Time.deltaTime));
+    }
+}
