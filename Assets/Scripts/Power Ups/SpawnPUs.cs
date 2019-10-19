a78d7031e8deb1eddb207d4e3fcf89d670f4a882
Jordan <38971636+jordanABurns@users.noreply.github.com>
2019-10-19 16:42:34 +1100

    update

--- Assets/Scripts/Power Ups/SpawnPUs.cs
+++ Assets/Scripts/Power Ups/SpawnPUs.cs
@@ -7,17 +7,33 @@ public class SpawnPUs : MonoBehaviour {
     public Vector3 center;
     public Vector3 size;
 
-	// Use this for initialization
-	void Start () {
-		
+    private int randomPU;
+
+    public GameObject[] PUs;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < 7; ++i)
+        {
+            spawnPUs();
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	   
 	}
 
-    private void OnDrawGizmosSelected()
+    public void spawnPUs()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x/2, size.x/2), Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.x / 2, size.x / 2));
+
+        randomPU = Random.Range(0,2);
+
+        Instantiate(PUs[randomPU], pos, Quaternion.identity);
+    }
+
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = new Color(1, 0, 0, 0.5f);
         Gizmos.DrawCube(center, size);
